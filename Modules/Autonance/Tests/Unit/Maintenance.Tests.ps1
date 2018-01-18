2428a73adf437d2d6fb16989c85c3972a0f04c37
Claudio Spizzi <claudio@spizzi.net>
2018-01-18 14:06:36 +0100

    Remove output test

--- Modules/Autonance/Tests/Unit/Maintenance.Tests.ps1
+++ Modules/Autonance/Tests/Unit/Maintenance.Tests.ps1
@@ -11,20 +11,6 @@ Describe 'Maintenance' {
 
         Mock 'Get-Help' -Verifiable { }
 
-        Mock 'Write-Autonance' -ModuleName $moduleName -ParameterFilter { $Message -eq "" -and $Type -eq 'Info' } -Verifiable { }
-        Mock 'Write-Autonance' -ModuleName $moduleName -ParameterFilter { $Message -eq "`n" -and $Type -eq 'Info' } -Verifiable { }
-        Mock 'Write-Autonance' -ModuleName $moduleName -ParameterFilter { $Message -eq "Maintenance Outer" -and $Type -eq 'Container' } -Verifiable { }
-        Mock 'Write-Autonance' -ModuleName $moduleName -ParameterFilter { $Message -like "Autonance Version *.*.*`nCopyright (c) 2017 by Claudio Spizzi. Licensed under MIT license." -and $Type -eq 'Info' } -Verifiable { }
-
-        It 'should show header and output' {
-
-            # Act
-            Maintenance 'Outer' { }
-
-            # Assert
-            Assert-MockCalled 'Write-Autonance' -ModuleName $moduleName -Times 4 -Exactly
-        }
-
         It 'should execute the script block' {
 
             # Act
